f3a397a1bf4318c84746f795ae87cb51bac47e09
leonva <leonv.allotey@gmail.com>
2025-11-16 18:26:05 -0600

    Delete build and .cache

--- build/CMakeFiles/freight.dir/src/main.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/freight.dir/src/main.cpp.o: \
- /home/lacer/projects/lang.cpp/freight/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/lacer/projects/lang.cpp/freight/src/cli.h \
- /usr/include/docopt/docopt.h /usr/include/c++/15.2.1/iostream \
- /usr/include/c++/15.2.1/string
